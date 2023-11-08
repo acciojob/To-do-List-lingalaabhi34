1b4018986dd639899527dc4e3b00f1d387c2bfa1
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-11-09 00:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
 
+document.addEventListener("DOMContentLoaded", function () {
+  const newTodoInput = document.getElementById("newTodoInput");
+  const addTodoBtn = document.getElementById("addTodoBtn");
+  const todoList = document.getElementById("todoList");
+
+  addTodoBtn.addEventListener("click", function () {
+    const todoText = newTodoInput.value.trim(); // Trim removes leading/trailing spaces
+
+    if (todoText !== "") {
+      // Check if the input is not empty
+      const listItem = document.createElement("li");
+      listItem.textContent = todoText;
+      todoList.appendChild(listItem);
+      newTodoInput.value = ""; // Clear the input field
+    }
+  });
+
+  newTodoInput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      addTodoBtn.click(); // Trigger the button click event when Enter is pressed
+    }
+  });
+});
